8844773e81ba888327a1bef46737c6cf5c36597f
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-09-23 16:56:16 +0100

    added a form to add links

--- server.rb
+++ server.rb
@@ -25,3 +25,10 @@ get '/' do
   @links = Link.all
   erb :index
 end
+
+post '/links' do
+  url = params["url"]
+  title = params["title"]
+  Link.create(:url => url, :title => title)
+  redirect to('/')
+end
