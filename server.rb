e45ed5d9c3289c8320632e6aadcc56c3d624fbcc
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-09-24 14:30:39 +0100

    Filtering the links by tag

--- server.rb
+++ server.rb
@@ -34,3 +34,9 @@ post '/links' do
   Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
 end
+
+get '/tags/:text' do
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
+  erb :index
+end
