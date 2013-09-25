f41a3a2b35451eadd0773e0abbc8e85aba481e90
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-09-25 10:51:50 +0100

    added password confirmation

--- lib/user.rb
+++ lib/user.rb
@@ -11,6 +11,16 @@ class User
   # and it's not enough for the hash and salt
   property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  # this is datamapper's method of validating the model.
+  # The model will not be saved unless both password
+  # and password_confirmation are the same
+  # read more about it in the documentation
+  # http://datamapper.org/docs/validations.html
+  validates_confirmation_of :password
+
   # when assigned the password, we don't store it directly
   # instead, we generate a password digest, that looks like this:
   # "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
@@ -18,6 +28,7 @@ class User
   # has both the password hash and the salt. We save it to the database
   # instead of the plain password for security reasons.
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -18,10 +18,15 @@ feature "User signs up" do
     expect(User.first.email).to eq("alice@example.com")        
   end
 
-  def sign_up(email = "alice@example.com", password = "oranges!")
+  scenario "with a password that doesn't match" do
+    lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)    
+  end
+
+  def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = 'oranges!')
     visit '/users/new'
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 
