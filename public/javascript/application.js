ea7568438f8b34907a8822b71c8ce1b82b1febb5
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-01 11:36:06 +0100

    storing favourited using data()

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,15 @@
 function addFavouritesHandler() {
-  $(".star.solid").click(function(event) {    
-    var newOpacity = 1 - parseInt($(this).css('opacity'));
+  $(".star.solid").click(function(event) {  
+    // get the link this star belongs to
+    var link = $(this).parent();
+    // get a boolean value for 'favourited'
+    // double negation casts any value to boolean
+    var favourited = !!$(link).data("favourited");
+    // decide what the opacity is going to be
+    var newOpacity = favourited ? 0 : 1;
+    // toggle the 'favourited' variable on the link element
+    $(link).data("favourited", !favourited);    
+    // perform the animation
     $(this).animate({opacity: newOpacity}, 1000);
   });
 }
