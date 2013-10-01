96ec233126d2b9a31d42ba1be24c6be05787cfd1
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-01 12:34:21 +0100

    creating a flash on the fly

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,18 @@
+function showLinkFavouritedNotice(link) {
+  var favourited = !!$(link).data("favourited");
+  // get the text of the .title element
+  // that we find among the children of link
+  var name = $(link).find('.title').text();
+  var message = favourited ?
+                name + " was added to favourites" :
+                name + " was removed from favourites";
+  var flash = $("<div></div>").addClass('flash').addClass('notice').html(message);
+  $(flash).appendTo('#flash-container');
+  setTimeout(function() {
+    $(flash).fadeOut();
+  }, 3000);
+}
+
 function addFavouritesHandler() {
   $(".star.solid").click(function(event) {  
     // get the link this star belongs to
@@ -11,6 +26,7 @@ function addFavouritesHandler() {
     $(link).data("favourited", !favourited);    
     // perform the animation
     $(this).animate({opacity: newOpacity}, 1000);
+    showLinkFavouritedNotice(link);
   });
 }
 
