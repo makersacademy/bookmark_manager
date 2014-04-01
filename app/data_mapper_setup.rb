c948b0a7985e1280f8971bc3e0f90d4d2e2ec31b
Antony Denyer <antonydenyer@gmail.com>
2014-04-01 18:02:07 +0100

    fix issues with rake file not migrating changes

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,12 @@
 env = ENV["RACK_ENV"] || "development"
 
-# we're telling datamapper to use a postgres database on localhost. 
-# The name will be "bookmark_manager_test" or "bookmark_manager_development" 
+
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
+
+# we're telling datamapper to use a postgres database on localhost.
+# The name will be "bookmark_manager_test" or "bookmark_manager_development"
 # depending on the environment
 # DataMapper::Logger.new(STDOUT, :debug)
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
