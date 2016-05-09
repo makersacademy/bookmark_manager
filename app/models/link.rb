f568437c3318d61155b3ae06ab4da5492b5cf9c5
DanLDB <dpbergquist@gmail.com>
2016-05-09 13:52:12 +0100

    13-configuring-the-rack-env

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
