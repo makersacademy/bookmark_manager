1e3776c3ab4681e9c4c12a397d5ccba46f80c551
DanLDB <dpbergquist@gmail.com>
2016-05-09 17:50:38 +0100

    23-user-sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String, required: true
+  property :email, String, required: true, unique: true
   property :password_digest, Text
 
   validates_confirmation_of :password
@@ -16,6 +16,15 @@ class User
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
   end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,7 +10,7 @@ feature 'User sign up' do
   scenario 'Cannot sign up a user without password confirmation' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
@@ -19,5 +19,13 @@ feature 'User sign up' do
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
