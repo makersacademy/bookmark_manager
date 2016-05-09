5d3dd27d9d04f1c26818f38dafa0e6bd3d8d880b
DanLDB <dpbergquist@gmail.com>
2016-05-09 16:43:24 +0100

    20-improving-sign-up-workflow

--- app/server.rb
+++ app/server.rb
@@ -1,11 +1,13 @@
 ENV["RACK_ENV"] ||="development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require './db/data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
   
   get '/links' do
     @links = Link.all
@@ -33,15 +35,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email:          params[:email],
-                password:              params[:password],
-                password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    @user = User.new(email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   set :root, File.dirname(__FILE__)
